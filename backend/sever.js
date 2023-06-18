e9923c3c492e3c271d968a91d055f02ab4cd04ec
aswathy999773 <aswathysuresh251@gmail.com>
2023-06-18 12:46:28 +0530

    updsate mern

--- backend/sever.js
+++ backend/sever.js
@@ -1,19 +1,31 @@
 const express = require('express');
 const app= express();
-const PORT = 4567
+const PORT = 1994
 const mongoose = require ('mongoose');
+const cors = require('cors'); // to remore cor issue
+app.use(cors())  // cor policy activation
+app.use(express.json()); // to render json req from frontend
+app.use(express.urlencoded({extended:true})); // to render form data from frontend
+
 
 mongoose.connect("mongodb+srv://aswathysuresh251:ayshumubi773@cluster0.255mxi0.mongodb.net/")
 .then(()=>{console.log("MongoDB connected successfully")})
 .catch((err)=>{console.log("Error connecting in MongoDB"+err)})
 
-app.get('/',(req,res)=>{
-    res.send('server is connecting')
+app.post('/addData', (req, res) => {
+    try {
+
+        let item = req.body
+        console.log(item)
+
+
+
+    } catch (error) {
+        res.send(error);
+    }
 })
 
-app.get('/about',(req,res)=>{
-    res.send('About')
-})
+
 app.listen(PORT,()=>{
  console.log(`listening on port ${PORT}`);
 });
